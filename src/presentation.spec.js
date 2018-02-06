aea9f6fce3221bcb7bfe9f6e8ed26e883aea078e
Johann Wagner <johann.wagner@codecentric.de>
2018-02-06 10:54:47 +0100

    Improved coverage for presentation component

--- src/presentation.spec.js
+++ src/presentation.spec.js
@@ -1,21 +1,44 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import Presentation from './presentation'
+import Slide from './slide'
+import Canvas from './canvas'
+
+const setupSpies = tap => {
+	return {
+		log: jest.spyOn(global.console, 'log').mockImplementation(() => tap()),
+		error: jest.spyOn(global.console, 'error').mockImplementation(() => tap())
+	}
+}
 
 describe('Presentation component', () => {
+	afterEach(() => {
+		jest.restoreAllMocks()
+	})
+
+	it('should allow forward/backward navigation', () => {
+		const {error, log} = setupSpies()
+		expect(error).not.toHaveBeenCalled()
+		expect(log).not.toHaveBeenCalled()
+
+		const component = shallow(<Presentation />)
+		component.instance().slidesLoadedCallback(5)
+		component.instance().handleNavigation('forward')
+		component.instance().handleNavigation('backward')
+
+		expect(error).not.toHaveBeenCalled()
+		expect(log).not.toHaveBeenCalled()
+	})
+
 	it('should not allow going backward when on the first slide', done => {
 		const component = shallow(<Presentation />)
 		component.instance().slidesLoadedCallback(5)
 
-		const consoleLog = console.log
-		console.log = jest.fn(() => {
-			done()
-		})
+		const {log} = setupSpies(done)
 		// precondition: nothing should've been logged thus far
-		expect(console.log).not.toHaveBeenCalled()
+		expect(log).not.toHaveBeenCalled()
 		component.instance().handleNavigation('backward')
-		expect(console.log).toHaveBeenCalled()
-		console.log = consoleLog
+		expect(log).toHaveBeenCalled()
 	})
 
 	it('should not allow going forward when on the last slide', done => {
@@ -23,16 +46,37 @@ describe('Presentation component', () => {
 		component.instance().slidesLoadedCallback(5)
 		component.setState({position: 4})
 
-		const consoleLog = console.log
-		console.log = jest.fn(() => {
-			done()
-		})
+		const {log} = setupSpies(done)
 		// precondition: nothing should've been logged thus far
-		expect(console.log).not.toHaveBeenCalled()
-
+		expect(log).not.toHaveBeenCalled()
 		component.instance().handleNavigation('forward')
+		expect(log).toHaveBeenCalled()
+	})
 
-		expect(console.log).toHaveBeenCalled()
-		console.log = consoleLog
+	it('should report an error when navigationCallback is called with an invalid parameter', done => {
+		const component = shallow(<Presentation />)
+
+		const {error} = setupSpies(done)
+		expect(error).not.toHaveBeenCalled()
+		component.instance().handleNavigation('Bogus')
+		expect(error).toHaveBeenCalled()
+	})
+
+	it('should not throw any errors for mixed element types primitive HTML/React', () => {
+		const {error, log} = setupSpies()
+		expect(error).not.toHaveBeenCalled()
+		expect(log).not.toHaveBeenCalled()
+
+		const component = shallow(
+			<Presentation>
+				<div>non-react element</div>
+				<Canvas>
+					<Slide />
+				</Canvas>
+			</Presentation>
+		)
+
+		expect(error).not.toHaveBeenCalled()
+		expect(log).not.toHaveBeenCalled()
 	})
 })
